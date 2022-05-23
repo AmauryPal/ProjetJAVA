3438468e8b46310bf1b4024497d4509e4ba863b2
AmauryPal <103425799+AmauryPal@users.noreply.github.com>
2022-05-23 08:04:40 +0200

    Add files via upload

---
+++ ProjetJAVA/src/application/ControllerGameName.java
@@ -0,0 +1,126 @@
+package application;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+
+public class ControllerGameName {
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	private static int whichSave = 1;
+	
+	@FXML
+	TextField gameName;
+	
+	public void switchToSaves(ActionEvent event) throws IOException {//switch vers la page des sauvegardes
+		String name = gameName.getText();
+		
+		if(!(name.isEmpty())) {//si le nom de la sauvegarde n'est pas vide
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Saves.fxml"));
+			root = loader.load();
+			
+			ControllerSaves controllerSaves = loader.getController();//on appelle la classe dans laquelle on est
+			ControllerMain.joueur.setGameName(name);
+			
+			setStopTime();//date de fin de partie
+			save("enCours");//on sauvegarde
+			controllerSaves.displayBDD();// afficher les infos dans la bdd
+			
+			
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			
+			scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(scene);
+			stage.show();
+		}
+	}
+	
+	public static void save(String where) {//sauvegarder l'instance joueur et board qui contiennent toutes les informations de notre partie.
+			
+		File directory=new File("./saves/"+where);
+		File[] logFiles = directory.listFiles();
+		
+		int fileCount = (logFiles == null) ? 0 : logFiles.length;//nombre de sauvegardes dispo dans enCours
+		
+		if(fileCount != 5) {//si il reste des emplacements vides
+			fileCount += 1;
+			try {
+		        FileOutputStream fileOut = new FileOutputStream("./saves/"+where+"/"+fileCount+".ser");
+		        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+		        out.writeObject(ControllerMain.getBoard());
+		        out.writeObject(ControllerMain.getPlayer());
+		        out.close();
+		        fileOut.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			}else {//sinon on prend le plus récent
+				Date oldDate = new Date();
+				for (int i = 1; i <= fileCount; i++) {//lire toutes les sauvegardes, puis comparer leurs dates
+				    try {
+					    ObjectInputStream in = new ObjectInputStream(new FileInputStream("./saves/"+where+"/"+i+".ser"));//on ouvre toutes les sauvegardes pour afficher les éléments
+					    @SuppressWarnings("unused")
+						int[][] board = (int[][]) in.readObject();
+					    Player joueur = (Player) in.readObject();
+					    in.close();
+					    
+					    if(joueur.getFullDate().before(oldDate)) {//oldDate = plus vielle date des 5 sauvegardes
+					    	oldDate = joueur.getFullDate();
+					    	whichSave = i;
+					    }
+					    
+				    } catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+		        FileOutputStream fileOut;
+				try {
+					fileOut = new FileOutputStream("./saves/"+where+"/"+whichSave+".ser");
+					ObjectOutputStream out = new ObjectOutputStream(fileOut);
+					
+					out.writeObject(ControllerMain.getBoard());
+					out.writeObject(ControllerMain.getPlayer());
+					out.close();
+					fileOut.close();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+	        
+		
+	}
+	public static void setStopTime() {
+		DateTimeFormatter FormatObj  = DateTimeFormatter.ofPattern("HH:mm:ss");//save hour
+		LocalTime temp = LocalTime.now();
+		String stopTime = temp.format(FormatObj);
+		ControllerMain.getPlayer().setStopTime(stopTime);
+	}
+
+}
---
+++ ProjetJAVA/src/application/ControllerMenuPrincipal.java
@@ -0,0 +1,213 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.Animation;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.animation.KeyFrame;
+import javafx.geometry.Bounds;
+import javafx.util.Duration;
+
+public class ControllerMenuPrincipal implements Initializable{
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+
+	public void switchToMain(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+		root = loader.load();
+		
+		ControllerMain controller = loader.getController();
+			 
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+		
+
+			@Override
+			public void handle(KeyEvent event) {
+
+				switch(event.getCode()) {
+
+				case UP:
+					controller.up();
+					break;
+				case DOWN:
+					controller.down();
+					break;
+				case LEFT:
+					controller.left();
+					break;
+				case RIGHT:
+					controller.right();
+					break;
+				case S:
+					controller.soluce();
+					break;
+				default:
+					break;
+				}		
+			}	
+		});
+		
+		stage.setScene(scene);
+		stage.show();
+		controller.init();
+		controller.go(event);
+	}
+	
+	
+	public void switchToMenuHistorique(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("menuHistorique.fxml"));
+		root = loader.load();
+			
+		ControllerMenuHistorique controller = loader.getController();
+		controller.displayBDD();
+		
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchToSaves(ActionEvent event) throws IOException {//page ou l'on écrit les saves
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("Saves.fxml"));
+		root = loader.load();
+		
+		ControllerSaves controller = loader.getController();
+		controller.displayBDD();
+		
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	@FXML
+	private Button logoutButton;
+	@FXML
+	private AnchorPane scenePane;
+	public void logout(ActionEvent event) {
+		stage = (Stage) scenePane.getScene().getWindow();
+		System.out.println("Vous avez quitté l'application avec succès !");
+		stage.close();
+	}
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private Circle circle;
+    @FXML
+    private Circle circle2;
+    @FXML
+    private Circle circle3;
+    @FXML
+    private Circle circle4;
+
+    //1 Frame evey 10 millis, which means 100 FPS
+    Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
+
+        double deltaX = 2;
+        double deltaY = 2;            
+        double deltaX2 = 2;
+        double deltaY2 = 2;
+        double deltaX3 = 2;
+        double deltaY3 = 2;            
+        double deltaX4 = 2;
+        double deltaY4 = 2;
+
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            circle.setLayoutX(circle.getLayoutX() + deltaX);
+            circle.setLayoutY(circle.getLayoutY() + deltaY);
+
+            Bounds bounds = pane.getBoundsInLocal();
+            boolean rightBorder = circle.getLayoutX() >= (bounds.getMaxX() - circle.getRadius());
+            boolean leftBorder = circle.getLayoutX() <= (bounds.getMinX() + circle.getRadius());
+            boolean bottomBorder = circle.getLayoutY() >= (bounds.getMaxY() - circle.getRadius());
+            boolean topBorder = circle.getLayoutY() <= (bounds.getMinY() + circle.getRadius());
+
+            if (rightBorder || leftBorder) {
+                deltaX *= -1;
+            }
+            if (bottomBorder || topBorder) {
+                deltaY *= -1;
+            }
+        
+
+        	circle2.setLayoutX(circle2.getLayoutX() - deltaX2);
+        	circle2.setLayoutY(circle2.getLayoutY() + deltaY2);
+        	
+        	Bounds bounds2 = pane.getBoundsInLocal();
+        	boolean rightBorder2 = circle2.getLayoutX() >= (bounds2.getMaxX() - circle2.getRadius());
+        	boolean leftBorder2 = circle2.getLayoutX() <= (bounds2.getMinX() + circle2.getRadius());
+        	boolean bottomBorder2 = circle2.getLayoutY() >= (bounds2.getMaxY() - circle2.getRadius());
+        	boolean topBorder2 = circle2.getLayoutY() <= (bounds2.getMinY() + circle2.getRadius());
+        	
+        	if (rightBorder2 || leftBorder2) {
+        		deltaX2 *= -1;
+        	}
+        	if (bottomBorder2 || topBorder2) {
+        		deltaY2 *= -1;
+        	}
+        	circle3.setLayoutX(circle3.getLayoutX() + deltaX3);
+        	circle3.setLayoutY(circle3.getLayoutY() - deltaY3);
+        	
+        	Bounds bounds3 = pane.getBoundsInLocal();
+        	boolean rightBorder3 = circle3.getLayoutX() >= (bounds3.getMaxX() - circle3.getRadius());
+        	boolean leftBorder3 = circle3.getLayoutX() <= (bounds3.getMinX() + circle3.getRadius());
+        	boolean bottomBorder3 = circle3.getLayoutY() >= (bounds3.getMaxY() - circle3.getRadius());
+        	boolean topBorder3 = circle3.getLayoutY() <= (bounds3.getMinY() + circle3.getRadius());
+        	
+        	if (rightBorder3 || leftBorder3) {
+        		deltaX3 *= -1;
+        	}
+        	if (bottomBorder3 || topBorder3) {
+        		deltaY3 *= -1;
+        	}
+        	circle4.setLayoutX(circle4.getLayoutX() - deltaX4);
+        	circle4.setLayoutY(circle4.getLayoutY() - deltaY4);
+        	
+        	Bounds bounds4 = pane.getBoundsInLocal();
+        	boolean rightBorder4 = circle4.getLayoutX() >= (bounds4.getMaxX() - circle4.getRadius());
+        	boolean leftBorder4 = circle4.getLayoutX() <= (bounds4.getMinX() + circle4.getRadius());
+        	boolean bottomBorder4 = circle4.getLayoutY() >= (bounds4.getMaxY() - circle4.getRadius());
+        	boolean topBorder4 = circle4.getLayoutY() <= (bounds4.getMinY() + circle4.getRadius());
+        	
+        	if (rightBorder4 || leftBorder4) {
+        		deltaX4 *= -1;
+        	}
+        	if (bottomBorder4 || topBorder4) {
+        		deltaY4 *= -1;
+        	}
+        }
+    
+    }));
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+}
